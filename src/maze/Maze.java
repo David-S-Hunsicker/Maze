2889bd172dcf42208e2df40946dc365a0b7d3ec2
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2013-11-06 12:53:40 -0500

    Update Maze.java

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,5 +1,39 @@
 package maze;
 
 public class Maze {
+Room[][] maze = new Room[][];
+Room finalExit;
+Room entrance;
+ArrayList<String> walls= new ArrayList<String>();
+private int x, y;
 
+
+public Maze(int rows, int columns){
+  x = rows;
+  y = columns;
+  Room[][] maze = new Room[x][y];
 }
+
+
+
+
+public void mazeMaker(){
+//create the maze by setting up rooms to have exits
+Room curRoom = maze[x][y];
+entrance = curRoom;
+
+for (int i = 0, p = 1; p < maze.length; i ++, p++){
+  walls.add(i+":"+p);
+}
+
+ for(int i = x; i< maze.length; i+=(y+1)){
+    walls.remove(i+":"+(i+1));
+ }
+
+for (int i = 0, p = (y+1), p<maze.length; i ++, p ++){
+  walls.add(i+":"+p);
+}
+
+
+}//end mazemaker
+}//end class
