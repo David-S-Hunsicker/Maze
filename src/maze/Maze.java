405bd4fc418c58b951b4b169224b093ead4b5c2a
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2013-11-06 17:21:21 -0500

    unfucked a bunch of stuff

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,12 +1,15 @@
 package maze;
 
+import java.util.ArrayList;
+
 public class Maze {
-Room[][] maze = new Room[][];
+static Room[][] maze;
 Room finalExit;
 Room entrance;
-ArrayList<String> walls= new ArrayList<String>();
-private int x, y;
-
+static ArrayList<String> walls= new ArrayList<String>();
+private static int rows;
+private static int columns;
+private static int rmNum;
 
 public Maze(int rows, int columns){
   this.rows = rows;
@@ -26,33 +29,33 @@ for (int i = 0, p = 1; p < maze.length; i ++, p++){
   walls.add(i+":"+p);
 }
 
- for(int i = x; i< maze.length; i+=(y+1)){
+ for(int i = rows; i< maze.length; i+=(columns+1)){
     walls.remove(i+":"+(i+1));
  }
 
-for (int i = 0, p = (y+1), p<maze.length; i ++, p ++){
+for (int i = 0, p = (columns+1); p<maze.length; i ++, p ++){
   walls.add(i+":"+p);
 }
 
 
 }//end wallMaker
 
-public mazeMaker(){
+public void mazeMaker(){
   int x,y;
   
   Room curRoom = maze[1][0];
   entrance = curRoom;
   int visited=0;
   
-  while(visited<maze.length()){
+  while(visited<maze.length){
     //i the current room has any exists
               //get room number, then check the arrayList for that number
             //parse all elements in the array list for the roomNumber
     // randomly select 1, or 3 walls and remove them
     // they must be removed from the pairs ArrayList;
     //set room.visisted=true;
-    //set teh current room to THAT room.
-    
+    //set the current room to THAT room.
+     
     curRoom = maze[0][0];
     rmNum= curRoom.roomNumber();
     //use a random generator to get either 1, 2, then 
@@ -71,15 +74,16 @@ public mazeMaker(){
   
   
 }//mazeMaker
+
 //go through each row, filling every column of the row, then
 // go to the next row.
 
   private void roomNumbers(){
-    number = 0;
+    int number = 0;
     
     for(int x =0; x < rows; x ++){
       for(int y =0;y<columns; y ++){
-      curRoom = maze[x][y];
+      Room curRoom = maze[x][y];
       curRoom.setRoomNumber(number);
       number++;
       }//y loop
