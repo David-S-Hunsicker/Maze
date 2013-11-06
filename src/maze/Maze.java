4d9ae75fd929257f1deff0d748362b37acb157ee
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2013-11-06 13:46:00 -0500

    Update Maze.java

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -5,7 +5,6 @@ Room[][] maze = new Room[][];
 Room finalExit;
 Room entrance;
 ArrayList<String> walls= new ArrayList<String>();
-int[] rmNmbr = new int[];
 private int x, y;
 
 
@@ -13,7 +12,6 @@ public Maze(int rows, int columns){
   this.rows = rows;
   this.columns = columns;
   Room[][] maze = new Room[rows][columns];
-  int[] rmNums =[(rows*columns)]
   
   wallMaker();
   roomNumbers();
@@ -44,16 +42,46 @@ public mazeMaker(){
   
   Room curRoom = maze[1][0];
   entrance = curRoom;
+  int visited=0;
   
-  
+  while(visited<maze.length()){
+    //i the current room has any exists
+              //get room number, then check the arrayList for that number
+            //parse all elements in the array list for the roomNumber
+    // randomly select 1, or 3 walls and remove them
+    // they must be removed from the pairs ArrayList;
+    //set room.visisted=true;
+    //set teh current room to THAT room.
+    
+    curRoom = maze[0][0];
+    rmNum= curRoom.roomNumber();
+    //use a random generator to get either 1, 2, then 
+    //search all elements of roomNumber ArrayList, remove that number, or until no more elements.
+    
+    //search for the walls in the array list
+    
+    
+    
+    
+    
+    }//while
   
   
   
   
 }//mazeMaker
-private void roomNumbers(){
-  for(int i = 0; i < rmNumbs.length; i++){
-    rmNumbs[i]= i;
-}//for
+//go through each row, filling every column of the row, then
+// go to the next row.
 
+  private void roomNumbers(){
+    number = 0;
+    
+    for(int x =0; x < rows; x ++){
+      for(int y =0;y<columns; y ++){
+      curRoom = maze[x][y];
+      curRoom.setRoomNumber(number);
+      number++;
+      }//y loop
+    }//x loop
+  }//roomNumbers
 }//end class
