5d0ae5d2c0623dd27fd2e889a5edd746756a7458
David-S-Hunsicker <david.s.hunsicker@gmail.com>
2013-11-10 18:48:48 -0500

    collaberated changes

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -1,6 +1,9 @@
+//Authors: Piotr Romanowski and David Hunsicker
+
 package maze;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Maze {
 static Room[][] maze;
@@ -18,13 +21,11 @@ public Maze(int rows, int columns){
   
   wallMaker();
   roomNumbers();
-}
-
-
-
+  mazeMaker();
+} 
 
 static private void wallMaker(){
-
+	
 for (int i = 0, p = 1; p < maze.length; i ++, p++){
   walls.add(i+":"+p);
 }
@@ -41,38 +42,48 @@ for (int i = 0, p = (columns+1); p<maze.length; i ++, p ++){
 }//end wallMaker
 
 public void mazeMaker(){
-  int x,y;
-  
+  //Create stack that will be used to extract just the integers
+  //from the wall list arraylist
   Room curRoom = maze[1][0];
   entrance = curRoom;
   int visited=0;
   
   while(visited<maze.length){
-    //i the current room has any exists
+	  
+	  curRoom = maze[0][0];
+    rmNum= curRoom.roomNumber();
+    
+    
+    int removals = (int) (Math.random()+1); 
+    for(int i = 0; i < removals; i ++){
+    	//parse walls array list to find relevant elements
+    	for (int s =0; s< walls.size(); s++){
+    		String pair = walls.get(s).toString();
+    		int num1 = Integer.parseInt(pair.split(":")[0]);//This is the int before ":"
+			int num2 = Integer.parseInt(pair.split(":")[1]);//This is the int after ":"
+    		if(rmNum ==num1 || rmNum == num2){
+    			walls.remove(s);
+    		}//If num1 or num2 are equal to the roomnumber, remove that wall
+    		
+	
+    	}//walls iterator
+    }//removal for loop
               //get room number, then check the arrayList for that number
             //parse all elements in the array list for the roomNumber
-    // randomly select 1, or 3 walls and remove them
+    // randomly select 1 or 2 walls and remove them
     // they must be removed from the pairs ArrayList;
     //set room.visisted=true;
     //set the current room to THAT room.
      
-    curRoom = maze[0][0];
-    rmNum= curRoom.roomNumber();
+    
     //use a random generator to get either 1, 2, then 
     //search all elements of roomNumber ArrayList,
     //parse the number from the string
     //remove that number, or until no more elements.
     
-    
-    
-    
-    
-    
+ 
     }//while
-  
-  
-  
-  
+   
 }//mazeMaker
 
 //go through each row, filling every column of the row, then
@@ -89,4 +100,7 @@ public void mazeMaker(){
       }//y loop
     }//x loop
   }//roomNumbers
+  
+
+  
 }//end class
